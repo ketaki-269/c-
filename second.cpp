020985fa7909d76881503887e6b22740bf14fd42
Ketaki Makode <ketakimakode@gmail.com>
2025-07-06 22:53:59 +0530

    New file

--- second.cpp
+++ second.cpp
@@ -2,7 +2,7 @@
 using namespace std ;
 int main()
 // online compiler will give 0 but vs code will give garbage value if we do not give value . 
-/*
+
 {
  int a=10 ; //declare and intialize
  cout<<a<<"\n";
@@ -11,34 +11,7 @@ cout<<a<<endl;   // endl is use to break the line
 cout<<"hello";
     
 }
-*/
-/*{
-    int a;
-   cout<<"Enter a number:";
-   cin>>a;
-   cout<<"The value you have entered:"<<a;
-return 0; 
-}*/
 
-{
-    int a=19;
-    cout<<a<<endl;
-
-    float b = 1.23;
-    cout<<b<<endl;
-    
-    char w='s';
-     cout <<w<<endl;
-     
-    bool c= true;
-    cout<<c<<endl;
-
-    double f =57.785989;
-    cout<<f<<endl;
-
-
-
-}
 
 // jo bhi kerra pure week usko achese revise kerna hai or code kerke dekh na hai for sunday
 
---
+++ third..cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+using namespace std ;
+int main()
+{
+    int a;
+   cout<<"Enter a number:";
+   cin>>a;
+   cout<<"The value you have entered:"<<a;
+return 0; 
+}
+
