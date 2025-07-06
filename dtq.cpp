9e00a7b6b2d349b4dcd835452c795e40bb4f5e39
Ketaki Makode <ketakimakode@gmail.com>
2025-07-06 23:36:53 +0530

    Updated file

---
+++ dtq.cpp
@@ -0,0 +1,36 @@
+// dtq- Data type Questions 
+
+// int give 5 basic interview quetions on  topic data types 
+#include<iostream> 
+using namespace std ;
+int main()
+
+// Q1. What will be the output of this code?
+// {
+//    int a = 7, b = 2;
+// int result = a / b;
+// cout << result;
+// }
+
+// Q 2. What happens if you store a decimal number in an int variable?
+// {
+//   int x = 7.8;
+// cout << x;
+  
+// }
+
+// 3. Predict the output and explain: 
+// {
+// int a = 2147483647; // Max value for 32-bit int
+// a = a + 1;
+// cout << a;
+// }
+
+// Q4. Write a simple code to find if a number is even or odd using int.
+// {
+// int num = 9;
+// if (num % 2 == 0)
+//     cout << "Even";
+// else
+//     cout << "Odd";
+// }
