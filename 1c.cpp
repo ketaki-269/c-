f49cf793f324d1dab6e0fb3f3bfd034384519b75
Ketaki Makode <ketakimakode@gmail.com>
2025-07-06 22:44:10 +0530

    first commit

---
+++ 1c.cpp
@@ -0,0 +1,12 @@
+// 3/7/25
+#include<iostream> 
+using namespace std ;
+int main()
+
+{
+    int a;  //int a        means this a  will take integer type data
+    cout<<" Enter NUmber :";
+    cin>>a;  //jo bhi no enter honga vo a me save hojainga
+    cout<<a;  //jo value a me save hai vo dispaly honga 
+    // return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+using namespace std ;
+int main()
+// online compiler will give 0 but vs code will give garbage value if we do not give value . 
+/*
+{
+ int a=10 ; //declare and intialize
+ cout<<a<<"\n";
+ a =20 ;
+cout<<a<<endl;   // endl is use to break the line 
+cout<<"hello";
+    
+}
+*/
+/*{
+    int a;
+   cout<<"Enter a number:";
+   cin>>a;
+   cout<<"The value you have entered:"<<a;
+return 0; 
+}*/
+
+{
+    int a=19;
+    cout<<a<<endl;
+
+    float b = 1.23;
+    cout<<b<<endl;
+    
+    char w='s';
+     cout <<w<<endl;
+     
+    bool c= true;
+    cout<<c<<endl;
+
+    double f =57.785989;
+    cout<<f<<endl;
+
+
+
+}
+
+// jo bhi kerra pure week usko achese revise kerna hai or code kerke dekh na hai for sunday
+
