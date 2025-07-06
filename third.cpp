cb03b6beb6f0aab2c5aa3c87dbd042be6647b1db
Ketaki Makode <ketakimakode@gmail.com>
2025-07-06 22:50:54 +0530

    New file

---
+++ third.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+using namespace std ;
+int main()
+{
+    int a=19;
+    cout<<a<<endl;
+
+    float b = 1.23;
+    cout<<b<<endl;
+    
+    char w='s';
+     cout <<w<<endl;
+     
+    bool c= true;
+    cout<<c<<endl;
+
+    double f =57.785989;
+    cout<<f<<endl;
+
+
+
+}
