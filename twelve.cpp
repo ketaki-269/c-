0acfc4c72e9ff03f5645a765c80434b36bd41d3c
Ketaki Makode <ketakimakode@gmail.com>
2025-07-12 16:31:27 +0530

    Updated file

---
+++ twelve.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//unary operator
+
+
+int main()
+
+
+// { 12-07-2025
+   // int a= 5;
+   // cout<<(++a)+(a++)<<endl;
+   // int a= 2;
+   // cout<<(a++)+(++a)<<endl;
+   //  int a= 3;
+   // cout<<(++a)+(++a)<<endl;
+   // int a= 5;
+   // cout<<(a++)+(++a)+(++a)<<endl;
+   // int a= 8;
+   // cout<<(++a)+(a++)+(a++)<<endl;          }
+
+   
