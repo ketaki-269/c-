c302f27401e7b8784029e00ec439bb3e3b7b11e7
Ketaki Makode <ketakimakode@gmail.com>
2025-07-17 17:34:19 +0530

    Updated file

---
+++ fourteen.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main()
+
+//unary operator again
+// 2 types of unary prefix & postfix 
+// prefix - pre increment ,  pre decrement
+// Postfix -   post increment ,  post decrement
+
+// {
+   
+//     int x=10;
+//     int y =2;
+//     int z;
+//     z=++y;
+//     y=z--;
+//     x=++z;
+//     cout<<x<<"\n";
+//     cout<<y<<"\n";
+//     cout<<z<<"\n";
+
+// }
+
+// {
+   
+//     int x=2;
+//     int y =1;
+//     int z;
+//     z=x--;
+//     x=z++;
+//     y=--z;
+//     cout<<x<<"\n";
+//     cout<<y<<"\n";
+//     cout<<z<<"\n";
+
+// }
+{
+ int x=-2;
+    int y =1;
+    int z;
+    z=x--;
+    x=z++;
+    y=--z;
+    cout<<x<<"\n";
+    cout<<y<<"\n";
+    cout<<z<<"\n";
+}
--- thirteen.cpp
+++ thirteen.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main ()
-
+//ternary operators
 
 
 // Find the greatest number between two?
