3fcdd16a1954e47abec8bf7aaf8c5183b69c27dd
Ketaki Makode <ketakimakode@gmail.com>
2025-07-21 17:00:36 +0530

    Updated file

--- seventeen.cpp
+++ seventeen.cpp
@@ -4,11 +4,19 @@ int main ()
 //ternary operators
 
 //  find the number is divisible by 10 or not ?
-{
-    int num;
-    cout<<"Enter a number";
-    cin>>num;
-    (num%10==0)?cout<<"Divisible" :cout<<" Not Divisible";
-}
+// {
+//     int num;
+//     cout<<"Enter a number";
+//     cin>>num;
+//     (num%10==0)?cout<<"Divisible" :cout<<" Not Divisible";
+// }
 
 //Check the givenn password is correct or not ?(password-1234)
+{
+    int num;
+    cout<<"Enter a Password";
+    cin>>num;
+    (num==1234)?cout<<"Correct Password":cout<<"Incorrect Password";  
+
+}
+//  Check the number is positive or negative
