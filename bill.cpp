143a32716ded17515dd4c1c92ea28adfbe746a39
Ketaki Makode <ketakimakode@gmail.com>
2025-07-25 18:41:03 +0530

    Updated file

---
+++ bill.cpp
@@ -0,0 +1,40 @@
+// Q-21 Calculate the bill 
+// 0-50 unit = 5 per unit
+// 50-100 unit= 10 per unit
+// 100- 150 unit = 15 per unit
+// 150 - 200 unit = 20 per unit
+// above 200 unit = 25 per unit 
+
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+int bill;
+cout<<"Enter a unit";
+cin>>bill;
+if(bill>=0 && bill<=50){
+    cout<<bill*5;}
+else if(bill>50 && bill<=100){
+    cout<<(bill*10)-250;}
+else if(bill>100 && bill<=150){
+    cout<<(bill*15)-750;}
+
+
+
+
+    else{
+        cout<<"Invaild";
+    }
+
+
+}
+
+
+
+// q - income tax Calculator
+// 0-10000 no tax
+// 10000- 50000 2% tax
+// 50000 - 1 lakh 5% tax
+// 1 lakh - 5 lakh 8 % tax
+// above 5 lakh 10% tax
---
+++ week.cpp
@@ -0,0 +1,36 @@
+// Q20 Print day of week given by user (1-7)?
+// 1-monday
+// 2-tuesday
+// 3-wednesday
+// 4-thrusday
+// 5-friday
+// 6-saturday
+// 7-sunday
+
+#include <iostream>
+using namespace std;
+int main ()
+
+{
+int num;
+cout<<"Enter a number";
+cin>>num;
+ if(num=1){
+    cout<<"Monday";}
+    else if(num=2){
+    cout<<"Tuesday";}
+  else if(num=3){
+    cout<<"Wednesday";}
+     else if(num=4){
+    cout<<"Thrusday";} 
+     else if(num=5){
+    cout<<"Friday";}
+     else if(num=6){
+    cout<<"Saturday";}  
+     else if(num=7){
+    cout<<"Sunday";}
+    else{
+        cout<<"Invaild";
+    }
+
+}
