18aa46202819f506dda02948989d54351319218c
Ketaki Makode <ketakimakode@gmail.com>
2025-07-26 00:02:24 +0530

    Updated file

--- income.cpp
+++ income.cpp
@@ -9,7 +9,6 @@ int main ()
 // 1 lakh - 5 lakh 8 % tax
 // above 5 lakh 10% tax
 
-
 {
 int num;
 cout<<"Enter a number";
@@ -18,15 +17,15 @@ if(num<=10000){
     cout<<num ;}
 
 else if(num>10000 && num<=50000){
-    cout<<num+(0.2*num);}
+    cout<<num+(0.02*num);}
 else if(num>50000 && num<=100000){
-    cout<<num+(0.5*num) ;}
+    cout<<num+(0.05*num) ;}
 
 else if(num>100000 && num<=500000){
-    cout<<num+(0.8*num)  ;}
+    cout<<num+(0.08*num)  ;}
 
 else if(num>500000){
-    cout<<num+(1*num) ;}    
+    cout<<num+(0.1*num) ;}    
    else {
             cout<<"Invalid";
         }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,1 @@
-{
-//     int num;
-//     cout<<"Enter a number";
-//     cin>>num;
-//     if(num%8==0 && num%10==0){
-//      cout<<"Divisible";}
-//        else{
-//        cout<<" Not Divisible";}
-// }
+00
