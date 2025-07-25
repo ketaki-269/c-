66a072421d8083ba2138319033f4453eeeda6587
Ketaki Makode <ketakimakode@gmail.com>
2025-07-25 21:58:13 +0530

    Updated file

--- bill.cpp
+++ bill.cpp
@@ -32,11 +32,3 @@ else if(bill>200){
 
 }
 
-
-
-// q - income tax Calculator
-// 0-10000 no tax
-// 10000- 50000 2% tax
-// 50000 - 1 lakh 5% tax
-// 1 lakh - 5 lakh 8 % tax
-// above 5 lakh 10% tax
