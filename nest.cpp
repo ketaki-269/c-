9b259dcb5ce2a1f0f174e7348ac62007ce34a31b
Ketaki Makode <ketakimakode@gmail.com>
2025-07-26 17:31:30 +0530

    Updated file

---
+++ nest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main ()
+
+ // nesting 
+ 
+// enter amazon or other is Invalid
+// then ask product or service
+// if they click service thencout not available yet
+// if the click product then cout shoes and clothes 
+// if they choose shoes cout 1000
+// or if the choose clothes cout 2000
+
+{
+ string website;
+ cout<<"Enter website name";
+ cin>>website;
+
+
+  if(website =="amazon"){
+    string option;
+    cout<<"Enter "
+   }
+
+  else{ cout<<"Invalid";}
+}
+
+
+
+
+
+
+//
