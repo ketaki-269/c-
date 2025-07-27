5f4799eb71b26290924deca134ca2275595615dc
Ketaki Makode <ketakimakode@gmail.com>
2025-07-28 01:12:54 +0530

    Updated file

--- income.cpp
+++ income.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main ()
 
-// q - income tax Calculator
+// q22 - income tax Calculator
 // 0-10000 no tax
 // 10000- 50000 2% tax
 // 50000 - 1 lakh 5% tax
--- week.cpp
+++ week.cpp
@@ -15,19 +15,19 @@ int main ()
 int num;
 cout<<"Enter a number";
 cin>>num;
- if(num=1){
+ if(num==1){
     cout<<"Monday";}
-    else if(num=2){
+    else if(num==2){
     cout<<"Tuesday";}
-  else if(num=3){
+  else if(num==3){
     cout<<"Wednesday";}
-     else if(num=4){
+     else if(num==4){
     cout<<"Thrusday";} 
-     else if(num=5){
+     else if(num==5){
     cout<<"Friday";}
-     else if(num=6){
+     else if(num==6){
     cout<<"Saturday";}  
-     else if(num=7){
+     else if(num==7){
     cout<<"Sunday";}
     else{
         cout<<"Invaild";
