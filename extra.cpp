dc41b89e43d9e3dc5d2809e4c395779b23e99cb9
Ketaki Makode <ketakimakode@gmail.com>
2025-07-30 16:53:17 +0530

    Updated file

---
+++ extra.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main ()
+//swapping
+// {
+//     int a =10;
+//     int b= 20;
+//     int c;
+//       c=a;
+//       a=b;
+//       b-c;
+//     cout<<a<<endl;
+// cout<<b<<endl;
+// }
+
+//swapping without taking third variable
+{
+    int a =10;
+    int b= 20;
+     a=a+b;
+     b=a-b;
+     a=a-b;
+    cout<<a<<endl;
+cout<<b<<endl;
+}
