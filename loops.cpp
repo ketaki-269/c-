4181b5c39e91173c547d58d597d034dc67fa6509
Ketaki Makode <ketakimakode@gmail.com>
2025-07-30 23:42:27 +0530

    Updated file

--- loops.cpp
+++ loops.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main(){
 //do while
-
+int a=1;
+while(a<=10){
+    cout<<a<<"";
+    a++;
+    
+}
 
 
 
@@ -27,10 +32,10 @@ int main(){
 
 //for loops 
     // Add number form 1 to 10
-    int ans=0;
-    for(int i=1; i<=10 ;i++){
-        ans = ans+i;
-    }
+    // int ans=0;
+    // for(int i=1; i<=10 ;i++){
+    //     ans = ans+i;
+    // }
 
-    cout<<ans;
+    // cout<<ans;
 }
