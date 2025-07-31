69e28113299c459077e4aa3b60736f50d85e3947
Ketaki Makode <ketakimakode@gmail.com>
2025-07-31 21:01:25 +0530

    Updated file

--- loopsque/one.cpp
+++ loopsque/one.cpp
@@ -9,11 +9,17 @@ int main()
 // }
 
 //2.print 10-1?
+// for (int i= 10;i>=1;i--){
+//     cout<<i<<endl;
+// }
 
-for (int i= 10;i>=1;i--){
+//print number from 1 to n?
+int n;
+cout<<"Enter number ";
+cin>>n;
+for(int i =1;i<=n; i++){
     cout<<i<<endl;
 }
-
 
 
 
