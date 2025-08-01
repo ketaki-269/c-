022567ef1be37fee1993c0d387fb728865f9cc80
Ketaki Makode <ketakimakode@gmail.com>
2025-08-01 09:28:00 +0530

    Updated file

--- loopsque/one.cpp
+++ loopsque/one.cpp
@@ -43,4 +43,25 @@ int main()
 //     if (i%2!=0)
 //     cout<<i<<endl;
 // }
+
+//Sum of even number?
+// int ans=0;
+//     for(int i=1; i<=10 ;i++){
+//         if(i%2==0)
+    
+//         ans = ans+i;
+//     }
+//     cout<<ans;
+
+//Sum of odd  number ?
+int ans=0;
+    for(int i=1; i<=10 ;i++){
+        if(i%2!=0)
+    
+        ans = ans+i;
+    }
+    cout<<ans;
+
+
+
 }
