84081896a6b4430b94bbb8289c59ba43c5b3544d
Ketaki Makode <ketakimakode@gmail.com>
2025-08-01 09:49:33 +0530

    Updated file

--- loopsque/second.cpp
+++ loopsque/second.cpp
@@ -5,18 +5,18 @@ int main(){
 
 
 //WAP   to print the multiplication table of 2 from i 1-10?
-// int num = 2;  
-//     for (int i = 1; i <= 10; ++i) {
-//         cout << num << " * " << i << " = " << num * i << endl;
-//     }
+int num = 2;  
+    for (int i = 1; i <= 10; ++i) {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
 
 //WAP to print table of number by taking uer input?
-// int num;
-// cout<<"Enter number ";
-// cin>>num;
-//     for (int i = 1; i <= 10; ++i) {
-//         cout << num << " * " << i << " = " << num * i << endl;
-//     }
+int num;
+cout<<"Enter number ";
+cin>>num;
+    for (int i = 1; i <= 10; ++i) {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
 
 //WAP to print number divisible by 5 between 10-20?
 
---
+++ loopsque/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+int num = 2;  
+    for (int i = 1; i <= 10; ++i) {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
