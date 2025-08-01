be2c0c3a1d855fe26856595c6b458a4b8c5551c3
Ketaki Makode <ketakimakode@gmail.com>
2025-08-01 09:56:04 +0530

    Updated file

---
+++ loopsque/ntable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 
+
+//WAP to print table of number by taking uer input?
+int num;
+cout<<"Enter number ";
+cin>>num;
+    for (int i = 1; i <= 10; ++i) {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
+
+}
---
+++ loopsque/twotable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 
+
+
+//WAP   to print the multiplication table of 2 from 1-10?
+int num = 2;  
+    for (int i = 1; i <= 10; ++i) {
+        cout << num << " * " << i << " = " << num * i << endl;
+    }
+
+
+
+}
