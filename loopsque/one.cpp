037d9451bc756750d85f87159e516f5a0554bd37
Ketaki Makode <ketakimakode@gmail.com>
2025-08-01 09:19:01 +0530

    Updated file

--- loopsque/one.cpp
+++ loopsque/one.cpp
@@ -14,14 +14,33 @@ int main()
 // }
 
 //print number from 1 to n?
-int n;
-cout<<"Enter number ";
-cin>>n;
-for(int i =1;i<=n; i++){
-    cout<<i<<endl;
-}
+// int n;
+// cout<<"Enter number ";
+// cin>>n;
+// for(int i =1;i<=n; i++){
+//     cout<<i<<endl;
+// }
+
+//print factorial of a number ?
+// int n,ans=1;
+// cout<<"Enter number ";
+// cin>>n;
+// for(int i =1;i<=n; i++){
+//     ans=ans*i;
+// }
+// cout<<ans<<endl;
 
 
+//Print even number from 1-10
+// for (int i = 1; i<=10;i++){
+//     if (i%2==0)
+//     cout<<i<<endl;
+// }
 
 
+//Print odd number from 1-10
+// for (int i = 1; i<=10;i++){
+//     if (i%2!=0)
+//     cout<<i<<endl;
+// }
 }
