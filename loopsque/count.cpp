e908ecb9c4e1b3b4d503739820412c2bf962fc79
Ketaki Makode <ketakimakode@gmail.com>
2025-08-02 15:03:18 +0530

    Updated file

--- loopsque/count.cpp
+++ loopsque/count.cpp
@@ -24,6 +24,28 @@
 
 
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int num, count = 0;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     if (num == 0) {
+//         count = 1;
+//     } else {
+       
+//         while (num != 0) {
+//             num = num / 10;
+//             count++;
+//         }
+//     }
+
+//     cout << "Number of digits: " << count;
+// }
+
+
 #include <iostream>
 using namespace std;
 
@@ -34,15 +56,13 @@ int main() {
 
     if (num == 0) {
         count = 1;
-    } else {
-        if (num < 0) {
-            num = -num; // manually convert negative to positive
-        }
+    } 
+       
         while (num != 0) {
             num = num / 10;
             count++;
         }
-    }
+  
 
     cout << "Number of digits: " << count;
 }
