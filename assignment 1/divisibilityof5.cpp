d5ab8b3e031bca5fbcbe7e0be302fc85faa5498c
Ketaki Makode <ketakimakode@gmail.com>
2025-08-02 23:34:54 +0530

    Updated file

---
+++ assignment 1/divisibilityof5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 
+
+//WAP to print number divisible by 5 between 10-20?
+
+ for (int i = 10;i <= 20;++i) {
+        if (i % 5 == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+
+}
