5a75b0a000f59fbe5374c25ef5f4bffda4a6ae54
Ketaki Makode <ketakimakode@gmail.com>
2025-08-02 17:34:27 +0530

    Updated file

---
+++ loopsque/five.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+// {
+//     int num, max = 0;
+//     cout << "Enter a number: ";
+//     cin >> num;
+
+//     while(num != 0)
+//     {
+//         int digit =num%10;
+//         if(digit>max){
+//             max=digit;
+
+//         }
+//         num/=10;
+       
+//     }
+//      cout<<max;
+// }
+
+
+
+
+{
+    int num, min=num  ;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while(num != 0)
+    {
+        int digit =num%10;
+        if(digit < min){
+           min=digit;
+
+        }
+        num/=10;
+       
+    }
+     cout<<min;
+}
