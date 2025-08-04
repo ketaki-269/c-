6b19918952034e2de5eea3987e56021d3b89051a
Ketaki Makode <ketakimakode@gmail.com>
2025-08-04 13:14:37 +0530

    Updated file

--- loopsque/assignment 1/twotable.cpp
+++ loopsque/assignment 1/twotable.cpp
@@ -4,7 +4,6 @@ int main(){
  
 
 
-//WAP   to print the multiplication table of 2 from 1-10?
 int num = 2;  
     for (int i = 1; i <= 10; ++i) {
         cout << num << " * " << i << " = " << num * i << endl;
