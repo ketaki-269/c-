6dfcf84c5f727545cff60fe7d5104416976e4513
Ketaki Makode <ketakimakode@gmail.com>
2025-08-05 00:28:27 +0530

    Updated file

--- loopsque/assignment 2/armstrong.cpp
+++ loopsque/assignment 2/armstrong.cpp
@@ -23,21 +23,36 @@ int main()
 
 // }
 
+// WAP a program for power of a number.
+//  {
+//     int num, power, result =1;
+//     cout <<"Enter number:";
+//     cin>>num;
+//     cout <<"Enter power:";
+//     cin>>power;
+
+//     for (int i  = 1; i <= power ; ++i ){
+//         result = result*num;
+//     }
+//     cout<<result;
+//  }
+
+// isko kahan declare kerna hi ?? 
+
+
 {
-    int num,real,result = 0, remainder;
-    cout<<"Enter a number";
+    int num, count=0;
+    cout <<"Enter number:";
     cin>>num;
-    real = num;
-     while (real != 0) {
-        remainder = real % 10;    
-        result += remainder * remainder * remainder; 
-        real /= 10;  
+   
+    for (int i  = 1; i <= num ; i++ ){
+      if(num % i ==0){
+      count++;
+      }
     }
-
-    if (result == num)
-        cout << num << " is Armstrong number." << endl;
+ if (count == 2)
+        cout << "Prime"<< endl;
     else
-        cout << num << "is  Not  Armstrong number." << endl;
+        cout << "Not a Prime " << endl;    
 
-}
-
+ }
