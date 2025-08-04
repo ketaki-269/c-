13d7d6b16b5aae9b2c98ff0a650c48e4fe932824
Ketaki Makode <ketakimakode@gmail.com>
2025-08-05 00:37:47 +0530

    Updated file

---
+++ loopsque/assignment 2/assignment3.cpp
@@ -0,0 +1,25 @@
+// WAP to print the first 10 natural numbers using a loop.  
+// #include <iostream>
+// using namespace std ;
+// int main()
+// {
+//     for ( int i=1; i<=10;i++){
+//     cout<<i;
+//     }
+
+// }
+
+// WAP to calculate the factorial of a number using a for loop. 
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num,result=1;
+  cout<<"Enter number :";
+  cin>>num;
+
+  for (int i = 1; i<=num; i++ ){
+ result=result*num;
+  }
+  cout<<result;
+}
