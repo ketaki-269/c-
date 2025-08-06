80a1fe7b50e4bad22a54cdae9ad7469a657a8b7f
Ketaki Makode <ketakimakode@gmail.com>
2025-08-06 21:19:07 +0530

    Updated file

--- loopsque/assignment 3/fibo.cpp
+++ loopsque/assignment 3/fibo.cpp
@@ -1,1 +1,18 @@
 // 2. Generate Fibonacci series up to a given term using loops. 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,fi=0,se=1;
+    cout<<"Enter a Number:";
+    cin>>num;
+
+    cout <<fi<<" "<<se<<" ";
+
+    for(int i = 1; i<=num-2; i++){
+        int nx = fi+se;
+        fi=se;
+        se=nx;
+        cout<<nx<<" ";
+    }
+}
--- loopsque/assignment 3/palinloops.cpp
+++ loopsque/assignment 3/palinloops.cpp
@@ -1,1 +1,22 @@
 // 1. Check if a number is a palindrome using loops. 
+ #include <iostream>
+using namespace std;
+
+int main() {
+    int num, real, reversed = 0, remainder;
+    cout << "Enter a number: ";
+    cin >> num;
+    real = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+
+    if (real == reversed)
+        cout << "Palindrome";
+    else
+        cout << "Not a palindrome";
+
+}
