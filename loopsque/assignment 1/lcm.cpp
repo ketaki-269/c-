49a9bc1a6d8b2749758e68537da09276c8c402f5
Ketaki Makode <ketakimakode@gmail.com>
2025-08-06 21:08:54 +0530

    Updated file

--- loopsque/assignment 1/lcm.cpp
+++ loopsque/assignment 1/lcm.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-using namesapce std;
+using namespace std;
 int main()
 {
-    int a, b;
+    int a, b , mx;
     cin>>a>>b;
-    int mx=(a>b)?cout<<a:cout<<b;
-  int  lcm = mx;
+   (mx=a>b)?cout<<a:cout<<b;
+   int l = mx;
     while(1){
         if(mx%a==0 && mx%b==0){
             break;
