e3dd1d22e55e88391cc4380c78f9c1dfb753e648
Ketaki Makode <ketakimakode@gmail.com>
2025-08-06 13:46:38 +0530

    Updated file

---
+++ loopsque/fibo1.cpp
@@ -0,0 +1,12 @@
+// fibonacci series with break?
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i =1; i<=10 ; i++0){
+        cout<<i<<" ";
+        if (i==5){
+            break;
+        }
+    }
+}
---
+++ loopsque/fibo2.cpp
@@ -0,0 +1,13 @@
+//fibonacci with continue?
+#include <iostream>
+using namespace std;
+int main()
+{
+      for (int i =1; i<=10 ; i++0){
+       
+        if(i ==5){
+            continue;
+        }
+        cout<<i<<" ";
+}
+}
