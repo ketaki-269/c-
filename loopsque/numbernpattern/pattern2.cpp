1f546a4155d972683c8760cd7febc4d5b736aa47
Ketaki Makode <ketakimakode@gmail.com>
2025-08-08 22:23:28 +0530

    Updated file

--- loopsque/numbernpattern/pattern2.cpp
+++ loopsque/numbernpattern/pattern2.cpp
@@ -20,7 +20,8 @@ int main()
             else{
                 cout<<"  ";} 
             }
-        cout<<endl;}
+        cout<<endl;
+    }
 
     for( int i=0 ; i<=127 ;i++){
         cout<<"The Ascii value of "<<i<<" is "<<char(i)<<endl;}
