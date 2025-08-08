298d8f285681f5c399b97a21ca9323cf9f95cea8
Ketaki Makode <ketakimakode@gmail.com>
2025-08-08 22:25:46 +0530

    Updated file

--- loopsque/numbernpattern/pattern1.cpp
+++ loopsque/numbernpattern/pattern1.cpp
@@ -6,7 +6,8 @@ int main()
     for(int r=1 ; r<=5 ; r++)
     {
 
-        for(int c=1 ; c<=5 ; c++){
+        for(int c=1 ; c<=5 ; c++)
+        {
 
             if(c<=r){
                 cout<<"*";
