d45fea11510674c222e005a2543d9ba9aae8b814
Ketaki Makode <ketakimakode@gmail.com>
2025-08-11 17:26:42 +0530

    Updated file

---
+++ loopsque/numbernpattern/switch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main(){
+   
+    int x;
+    cout<<"Enter a Number: ";
+    cin>>x;
+
+    switch(x + x){
+        
+        case 1:
+        cout<<"Monday";
+        break;
+
+        case 2:
+        cout<<"Tuesday";
+        break;
+
+        case 3:
+        cout<<"Wednesday";
+        break;
+
+        default:
+        cout<<"Invalid";
+
+    }
+
+}
