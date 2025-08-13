cdb85382b0a77020667ba494364fce72efb2e32c
Ketaki Makode <ketakimakode@gmail.com>
2025-08-13 06:25:38 +0530

    Updated file

---
+++ loopsque/numbernpattern/patternnum2.cpp
@@ -0,0 +1,25 @@
+// 1    
+// 22
+// 333
+// 4444
+// 55555
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int r=1 ; r<=5 ; r++)
+    {
+
+        for(int c=1 ; c<=5 ; c++)
+        {
+
+            if(c<=r){
+                cout<<r;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
---
+++ loopsque/numbernpattern/patternnum3.cpp
@@ -0,0 +1,30 @@
+// Enter a number5
+// 1    
+// 23
+// 456
+// 78910 
+// 1112131415
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,a=1;
+    cout<<"Enter a number";
+    cin>>num;
+    for(int r=1 ; r<=num ; r++)
+    {
+
+        for(int c=1 ; c<=num ; c++)
+        {
+
+            if(c<=r){
+                cout<<a++ ;
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
