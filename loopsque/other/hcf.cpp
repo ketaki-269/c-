356badf7967999a24dd3722b20f0fd9192029f4d
Ketaki Makode <ketakimakode@gmail.com>
2025-08-15 22:34:11 +0530

    Updated file

--- loopsque/other/hcf.cpp
+++ loopsque/other/hcf.cpp
@@ -4,7 +4,10 @@ using namespace std;
 int main()
 {
     int a,b,hcf;
-    cin>>a>>b;
+    cout<<"Enter a:";
+    cin>>a;
+    cout<<"Enter b:";
+    cin>>b;
     for(int i=1;i<=a && i<=b;i++){
         if(a%i==0 && b%i==0){
             hcf=i;
@@ -12,3 +15,4 @@ int main()
     }
     cout<<hcf<<" ";
 }
+
--- loopsque/other/lcm.cpp
+++ loopsque/other/lcm.cpp
@@ -18,3 +18,4 @@ int main()
     }
     cout<<mx<<endl;
 }
+
