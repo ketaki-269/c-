76e861ad2c94ee6e7bd66c9738467e6541cb0bd0
Ketaki Makode <ketakimakode@gmail.com>
2025-08-16 14:29:05 +0530

    Updated file

--- loopsque/numbernpattern/armstrong.cpp
+++ loopsque/numbernpattern/armstrong.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 
 int main() 
+
 // {
 //     int num, real, result = 0, remainder;
 //     cout << "Enter a number: ";
 //     cin >> num;
 //     real = num;
-
-   
 //     while (real != 0) {
 //         remainder = real % 10;    
 //         result += remainder * remainder * remainder; 
 //         real /= 10;  
 //     }
-
 //     if (result == num)
 //         cout << num << " is Armstrong number." << endl;
 //     else
 //         cout << num << "is  Not  Armstrong number." << endl;
-
 // }
 
 // WAP a program for power of a number.
@@ -36,7 +33,6 @@ int main()
 //     }
 //     cout<<result;
 //  }
-
 // isko kahan declare kerna hi ?? 
 
 
