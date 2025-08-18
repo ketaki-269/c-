896b616650665ec2439b747c2cf80a37bd1d2801
Ketaki Makode <ketakimakode@gmail.com>
2025-08-18 05:31:14 +0530

    Updated file

---
+++ guess.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int  guess,number =8;
+    cout <<" Guess the number under 10 : ";
+        cin>>guess;
+
+        if (guess== number){
+            cout<<"You guessed right"<<endl;
+        }
+        else if (guess< number ){
+            cout <<"Too low ,try a bigger number"<<endl;
+        }
+            else{
+               cout <<"Too high ,try a smaller number"<<endl;
+
+            }
+        
+}
