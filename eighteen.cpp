0a489c5a9015fd58dac88165679b60dc3083cc4c
Ketaki Makode <ketakimakode@gmail.com>
2025-08-20 05:34:27 +0530

    Updated file

--- eighteen.cpp
+++ eighteen.cpp
@@ -8,7 +8,7 @@ int main ()
 //  int num;
 //     cout<<"Enter a number";
 //     cin>>num;
-//     {
+//     {                                                                                                                                                                                                                                                         
 //     if(num%10==0){
 //     cout<<"Divisible";}
 //         else{
