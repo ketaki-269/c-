b36497500011d8bbd973b928b78495772727fa00
Ketaki Makode <ketakimakode@gmail.com>
2025-09-02 13:47:19 +0530

    Updated file

--- loopsque/assignment 1/divisibilityof5.cpp
+++ loopsque/assignment 1/divisibilityof5.cpp
@@ -5,7 +5,8 @@ int main(){
 
 //WAP to print number divisible by 5 between 10-20?
 
- for (int i = 10;i <= 20;++i) {
+ for (int i = 10;i <= 20;++i) 
+ {
         if (i % 5 == 0)
          {
             cout << i << " ";
