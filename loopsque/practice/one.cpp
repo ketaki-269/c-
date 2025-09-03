ad9a65e63dd11d8deb0e5a97aae6bfa660e14e94
Ketaki Makode <ketakimakode@gmail.com>
2025-09-03 14:02:59 +0530

    Updated file

---
+++ loopsque/practice/one.cpp
@@ -0,0 +1,12 @@
+#include&lt;iostream.h&gt;
+ int main()
+{
+if(NULL)
+cout&lt;&lt;&quot;NULL&quot;;
+else if(FALSE)
+cout&lt;&lt;&quot;TRUE&quot;;
+else
+cout&lt;&lt;&quot;FALSE&quot;;
+return 0;
+}
+
